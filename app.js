915ad1b771a83d83128ea9bc38f63e544fe34706
yara hammami <yara.hammami13@gmail.com>
2022-07-12 08:37:05 +0300

    program version 1.0

---
+++ app.js
@@ -0,0 +1,11 @@
+const title = prompt("Enter the title of the table:");
+ document.querySelector("h1").innerHTML = title;
+
+ do{
+
+ const prosInput = prompt("Enter the pro you wish to add:");
+ const consInput = prompt("Enter the con you wish to add:");
+ document.querySelector("table").innerHTML += '<tr><td><h4>'+prosInput+'</h4></td><td><h4>'+consInput+'</h4></td></tr>';
+
+ 
+}while(consInput !="");
